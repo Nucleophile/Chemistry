b7a6190ec11db15d38cf069a0957b1f06a034cfa
Zanuda <zanuda.brat.zarazy@gmail.com>
2023-11-13 21:32:12 +0200

    Fixed some logic

--- src/composables/proceed.js
+++ src/composables/proceed.js
@@ -2,26 +2,15 @@ export default function useProceed(reactions, result) {
   return function proceed() {
     const addends = getAddends(reactions.value);
     result.value = {};
-    console.log(reactions.value);
     reactions.value.forEach((reaction, reactionNumber) => {
       for (let halfReaction in reaction.equation) {
         reaction.equation[halfReaction].forEach((participant) => {
           if (!result.value[participant.substance]) {
-            result.value[participant.substance] = `
-              <math>
-                <mfrac>
-                  <msub>
-                    <mi>&part;C</mi>
-                    <mi>${participant.substance}</mi>
-                  </msub>
-                  <mi>&part;&tau;</mi>
-                </mfrac>
-                <mo>=</mo>
-            `;
+            result.value[participant.substance] = "";
           }
 
           result.value[participant.substance] += getAddendsWithSignAndCoef(
-            result.value[participant.substance].rightPart,
+            result.value[participant.substance],
             reactionNumber,
             halfReaction,
             reaction.reversible,
@@ -32,9 +21,19 @@ export default function useProceed(reactions, result) {
       }
     });
     for (let substance in result.value) {
-      result.value[substance] += "</math>";
+      result.value[substance] = `
+      <math>
+        <mfrac>
+          <msub>
+            <mi>&part;C</mi>
+            <mi>${substance}</mi>
+          </msub>
+          <mi>&part;&tau;</mi>
+        </mfrac>
+        <mo>=</mo>
+        ${result.value[substance]}
+      </math>`;
     }
-    console.log(result);
   };
 }
 
@@ -50,7 +49,6 @@ function getAddendsWithSignAndCoef(
   participantCoef = participantCoef === 1 ? "" : `<mn>${participantCoef}</mn>`;
 
   if (halfReaction === "reactants") {
-    // const space = currentValue ? " " : ""; // if current addend is first in response string, we don't need " " between  minus and text
     addendWithSignAndCoef =
       "<mo>&minus;</mo>" + participantCoef + addends[reactionNumber][0];
   } else {
@@ -67,7 +65,7 @@ function getAddendsWithSignAndCoef(
         "<mo>&minus;</mo>" + participantCoef + addends[reactionNumber][1];
     }
   }
-
+console.log(addendWithSignAndCoef)
   return addendWithSignAndCoef;
 }
 
